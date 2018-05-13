aa009a6479de7bb47e8f80bae6e4ecbb27256202
Brave Pham <dungpa93@gmail.com>
2018-05-13 22:19:24 +0700

    adding sidebar, more administration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
+  get 'admin' => 'admin#index'
+  controller :sessions do 
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy 
+  end
+
   resources :products do
     get :who_bought, on: :member
   end
